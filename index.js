247ddcd0fb3a8a09ca60c3a7afe4160f2ee14115
Eric Thvedt <ericsthvedt@gmail.com>
2023-02-06 10:14:23 -0800

    all functions work and pass tests

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(arr, str) {
+    return arr.filter(el => {
+        return el.toLowerCase().includes(str.toLowerCase());
+    });
+};
+
+function fuzzyMatch(arr, str) {
+    return arr.filter(el => {
+        return el.startsWith(str);
+    });
+};
+
+function matchName(arr, str) {
+    return arr.filter(obj => {
+        return obj.name === str;
+    })
+}
